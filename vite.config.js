03678228751eef0110db938e3cd46a6573d91990
Panupong Keawkhao <panupongtongfah@gmail.com>
2023-11-19 02:20:13 +0700

    Update repo

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
   },
-  base: "/ESD-Suit-Detection-using-TensorFlow.js/",
+  base: "/esd-suit-detection-using-tensor-flow-js/",
 });
